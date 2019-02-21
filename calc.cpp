1d6e53321eebf309d16e2452acf546a17d1d1856
Ingit <ingitag777@gmail.com>
2019-02-21 10:33:47 +0530

    function mult

--- calc.cpp
+++ calc.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 void add();
 void sub();
+void mult();
+	void mult()
+{
+	int p,q;
+	cout<<"Enter two nos.\n"
+		cout<<"Multiplication : "<<a*b;
+}
+
 void sub()
 {
 int x,y;
@@ -17,7 +25,27 @@ void add()
 }
 int main()
 {
+	int n;
+	cout<<"This is a four function Calculator\n";
+	while(1)
+	{
+	cout<<"\n1.Addition\n2.Substraction\n3.Multiplication\n4.Division\n5.Exit\nEnter your choice\n"
+		cin>>n;
+	switch(n)
+	{
+	case 1:
 add();
+break;
+	case 2:
+
 sub();
+break;
+	case 3:
+
+mult();
+break;
+
+	}
+}
 return 0;
 }
