281f9dcc323f4c8adfd65ed327ab9307d5f71bfa
Ingit <ingitag777@gmail.com>
2019-04-07 19:41:16 +0530

    celcius to fahrenheit

---
+++ s3.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+float cl,f;
+printf("\nEnter the temperature in celsius");
+scanf("%f",&cl);
+f=(1.8*cl)+32;
+printf("\nTemperature in Fahrenheit= %f",f);
+return 0;
+}
